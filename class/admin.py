785adb937e4b84639947365561ef919290c7469f
Wachira-joy <wachiraglorywairimu@gmail.com>
2024-06-24 12:09:34 +0300

    added the models and updated them

---
+++ class/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Classs
+admin.site.register(Classs)
+
---
+++ teacher/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    code = models.PositiveSmallIntegerField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    salary = models.BigIntegerField()
+    hire_date = models.DateTimeField()
+    gender = models.CharField(max_length=10)
+    bio = models.TextField
+    pic = models.ImageField(max_length=20)
